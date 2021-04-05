5cd89567c69ed195e6a0f1036459ce34b09b940c
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2021-04-04 21:47:31 -0300

    Criando o Context API para tratar as transações - E completando a inserção de uma nova transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,11 @@
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
 import { Container, TransactionTypeContainer, TransactionTypeButton } from './styles';
 
 import closeImg from '../../assets/img/close.svg';
 import incomeImg from '../../assets/img/income.svg';
 import outcomeImg from '../../assets/img/outcome.svg';
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -13,17 +13,24 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {title, value, category, type};
+    await createTransaction({title, amount, type, category});
 
-    api.post('/transactions', data);
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+
+    onRequestClose();
   }
 
   return (
@@ -50,8 +57,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import incomeIcon from '../../assets/img/income.svg';
 import outcomeIcon from '../../assets/img/outcome.svg';
 import totalIcon from '../../assets/img/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from './styles';
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
   return (
     <Container>
       <div>
